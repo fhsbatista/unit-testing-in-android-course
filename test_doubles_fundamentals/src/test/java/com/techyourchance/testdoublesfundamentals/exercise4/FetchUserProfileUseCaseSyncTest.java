e5e86a8f10628189bdfbc1e3f759c38eba122fee
Fernando Batista <fernando.batista@mobile2you.com.br>
2019-01-30 09:04:32 -0200

    Initiate tests of exercice 5, instatianting classes as level class fields

--- test_doubles_fundamentals/src/test/java/com/techyourchance/testdoublesfundamentals/exercise4/FetchUserProfileUseCaseSyncTest.java
+++ test_doubles_fundamentals/src/test/java/com/techyourchance/testdoublesfundamentals/exercise4/FetchUserProfileUseCaseSyncTest.java
@@ -1,5 +1,55 @@
 package com.techyourchance.testdoublesfundamentals.exercise4;
 
+import com.techyourchance.testdoublesfundamentals.example4.networking.NetworkErrorException;
+import com.techyourchance.testdoublesfundamentals.exercise4.networking.UserProfileHttpEndpointSync;
+import com.techyourchance.testdoublesfundamentals.exercise4.users.User;
+import com.techyourchance.testdoublesfundamentals.exercise4.users.UsersCache;
+
+import org.jetbrains.annotations.Nullable;
+import org.junit.Before;
+
 public class FetchUserProfileUseCaseSyncTest {
+
+    private FetchUserProfileUseCaseSync SUT;
+    private UserProfileHttpSyncTD userProfileHttpSyncTD;
+    private UsersCacheTD usersCacheTD;
+
+
+    @Before
+    public void setup(){
+
+        userProfileHttpSyncTD = new UserProfileHttpSyncTD();
+        usersCacheTD = new UsersCacheTD();
+        SUT = new FetchUserProfileUseCaseSync(userProfileHttpSyncTD, usersCacheTD);
+
+        //when I call getUserProfile method
+
+    }
+
+
+
+
+    //----------------------------------------------------------------------------------------------
+    //Helper classes
+
+    private static class UserProfileHttpSyncTD implements UserProfileHttpEndpointSync {
+        @Override
+        public EndpointResult getUserProfile(String userId) throws NetworkErrorException {
+            return null;
+        }
+    }
+
+    private static class UsersCacheTD implements UsersCache {
+        @Override
+        public void cacheUser(User user) {
+
+        }
+
+        @Nullable
+        @Override
+        public User getUser(String userId) {
+            return null;
+        }
+    }
 
 }
