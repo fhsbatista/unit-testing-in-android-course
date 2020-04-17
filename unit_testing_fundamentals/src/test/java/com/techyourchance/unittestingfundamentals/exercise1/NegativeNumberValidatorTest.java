8b7d29c137c60763827f931a74441454a0c98ff6
Fernando Batista <fernandohsbatista@gmail.com>
2020-04-17 16:17:44 -0300

    finished the first unit test

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,36 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
 public class NegativeNumberValidatorTest {
+
+    NegativeNumberValidator sut;
+
+    @Before
+    public void setup() {
+        sut = new NegativeNumberValidator();
+    }
+
+    @Test
+    public void test1() {
+        boolean result = sut.isNegative(3);
+        Assert.assertThat(result, is(false));
+    }
+
+    @Test
+    public void test2() {
+        boolean result = sut.isNegative(0);
+        Assert.assertThat(result, is(false));
+    }
+
+    @Test
+    public void test3() {
+        boolean result = sut.isNegative(-2);
+        Assert.assertThat(result, is(true));
+    }
 
 }
