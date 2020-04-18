9996b6e82f2f3dceba9a88a52b521e6eb2a59551
Fernando Batista <fernandohsbatista@gmail.com>
2020-04-18 18:09:51 -0300

    finished second exercise

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,31 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class StringDuplicatorTest {
 
+    StringDuplicator sut;
+
+    @Before
+    public void setup() {
+        sut = new StringDuplicator();
+    }
+
+    @Test
+    public void stringDuplicator_emptyString_emptyStringReturned() {
+        String result = sut.duplicate("");
+        assertThat(result, is(""));
+    }
+
+    @Test
+    public void stringDuplicator_string_sameStringTwiceReturned() {
+        String result = sut.duplicate("fernando");
+        assertThat(result, is("fernandofernando"));
+    }
 }
